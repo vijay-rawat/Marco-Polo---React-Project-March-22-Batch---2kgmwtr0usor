cf62325db4198e0093eb43f88a59663fc1f5417f
Vijay Rawat <30477642+vijay-rawat@users.noreply.github.com>
2022-10-09 19:40:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
-  
+  const [getState, setState] = useState(false);
+
+  const toggleText = () => {
+    setState(!getState);
+  };
+
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{getState ? "Polo" : "Marco"}</h1>
+      <button id="marco-polo-toggler" onClick={toggleText}>
+        {getState ? "Marco" : "Polo"}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
